5ebce52cb0d2e3d19e974df40d714144200a49fc
CaMnter <yuanyu.camnter@gmail.com>
2016-03-06 16:49:03 +0800

    Well done

--- easyarcloading/src/main/java/com/camnter/easyarcloading/EasyArcLoading.java
+++ easyarcloading/src/main/java/com/camnter/easyarcloading/EasyArcLoading.java
@@ -1,18 +1,16 @@
 package com.camnter.easyarcloading;
 
 import android.animation.TypeEvaluator;
-import android.animation.ValueAnimator;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.os.Build;
+import android.os.SystemClock;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
-import android.view.animation.AccelerateDecelerateInterpolator;
-import android.view.animation.AccelerateInterpolator;
 
 /**
  * Description：EasyArcLoading
@@ -32,26 +30,6 @@ public class EasyArcLoading extends View {
 
     // Default px
     private float defaultViewPadding;
-
-    private static final float[] DEFAULT_EXTERNAL_DECREASE_START_ANGLE = {
-            0.0f, 246.0f, 360.0f
-    };
-    private static final float[] DEFAULT_EXTERNAL_DECREASE_SWEEP_ANGLE = {
-            1.0f, 240.0f, 340.0f
-    };
-    private static final float[] DEFAULT_INTERNAL_DECREASE_START_ANGLE = {
-            0.0f, 246.0f, 360.0f
-    };
-    private static final float[] DEFAULT_INTERNAL_DECREASE_SWEEP_ANGLE = {
-            1.0f, 240f, 270f
-    };
-
-
-    private static final int DECREASE_MAX_LENGTH = DEFAULT_EXTERNAL_DECREASE_START_ANGLE.length;
-    private static final int INCREASE_MAX_LENGTH = DEFAULT_INTERNAL_DECREASE_START_ANGLE.length;
-    private Object[] decreaseArc = new ArcProperty[DECREASE_MAX_LENGTH];
-    private Object[] increaseArc = new ArcProperty[INCREASE_MAX_LENGTH];
-
     private float arcPadding;
 
     private Paint externalPaint = new Paint();
@@ -60,11 +38,31 @@ public class EasyArcLoading extends View {
     private RectF externalRectF = new RectF();
     private RectF internalRectF = new RectF();
 
-    private float currentExternalStart;
-    private float currentExternalSweep;
+    private long lastTimeAnimated = 0;
 
-    private float currentInternalStart;
-    private float currentInternalSweep;
+    private static final float EXTERNAL_SPIN_SPEED = 600.0f;
+    private static final double EXTERNAL_INCREASE_MAX_TIME = 100.0d;
+    private static final double EXTERNAL_ANIMATION_TIME = 600.0d;
+
+    private static final float INTERNAL_SPIN_SPEED = 200.0f;
+    private static final double INTERNAL_INCREASE_MAX_TIME = 300.0d;
+    private static final double INTERNAL_ANIMATION_TIME = 600.0d;
+
+
+    private static final int ARC_MAX_LENGTH = 276;
+    private static final int ARC_MIN_LENGTH = 2;
+
+    private double externalIncreaseTime = 0.0d;
+    private double externalPastTime = 0.0d;
+    private boolean externalIncrease = true;
+    private float externalLength = 0.0f;
+    private float externalProgress = 0.0f;
+
+    private double internalIncreaseTime = 0.0d;
+    private double internalPastTime = 0.0d;
+    private boolean internalIncrease = true;
+    private float internalLength = 0.0f;
+    private float internalProgress = 0.0f;
 
 
     public EasyArcLoading(Context context) {
@@ -93,8 +91,12 @@ public class EasyArcLoading extends View {
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     private void init(Context context, AttributeSet attrs) {
+        this.externalPastTime = 0.0f;
+        this.internalPastTime = 0.0f;
+
         this.defaultViewPadding = this.dp2px(DEFAULT_VIEW_PADDING_DP);
         this.arcPadding = this.dp2px(DEFAULT_ARC_PADDING_DP);
+        this.lastTimeAnimated = SystemClock.uptimeMillis();
 
         this.externalPaint.setStrokeWidth(this.dp2px(DEFAULT_EXTERNAL_ARC_WIDTH));
         this.externalPaint.setAntiAlias(true);
@@ -109,13 +111,6 @@ public class EasyArcLoading extends View {
 
         this.setupRectF();
 
-        for (int i = 0; i < DEFAULT_EXTERNAL_DECREASE_START_ANGLE.length; i++) {
-            this.decreaseArc[i] = new ArcProperty(DEFAULT_EXTERNAL_DECREASE_START_ANGLE[i], DEFAULT_EXTERNAL_DECREASE_SWEEP_ANGLE[i]);
-        }
-        for (int i = 0; i < DEFAULT_INTERNAL_DECREASE_START_ANGLE.length; i++) {
-            this.increaseArc[i] = new ArcProperty(DEFAULT_INTERNAL_DECREASE_START_ANGLE[i], DEFAULT_INTERNAL_DECREASE_SWEEP_ANGLE[i]);
-        }
-
     }
 
     @Override
@@ -123,8 +118,32 @@ public class EasyArcLoading extends View {
         this.externalPaint.setStrokeCap(Paint.Cap.ROUND);
         this.internalPaint.setStrokeCap(Paint.Cap.ROUND);
 
-        canvas.drawArc(this.externalRectF, this.currentExternalStart, this.currentExternalSweep, false, this.externalPaint);
-        canvas.drawArc(this.internalRectF, this.currentInternalStart, this.currentInternalSweep, false, this.internalPaint);
+
+        long timeInterval = (SystemClock.uptimeMillis() - this.lastTimeAnimated);
+        float externalIncreaseProgress = timeInterval * EXTERNAL_SPIN_SPEED / 1000.0f;
+        float internalIncreaseProgress = timeInterval * INTERNAL_SPIN_SPEED / 1000.0f;
+
+        this.updateLength(timeInterval);
+
+        this.externalProgress += externalIncreaseProgress;
+        if (this.externalProgress > 360)
+            this.externalProgress -= 360f;
+
+        this.internalProgress += internalIncreaseProgress;
+        if (this.internalProgress > 360)
+            this.internalProgress -= 360f;
+
+        this.lastTimeAnimated = SystemClock.uptimeMillis();
+
+        float externalFrom = this.externalProgress - 120.0f;
+        float externalLength = this.externalLength + ARC_MIN_LENGTH;
+        float internalFrom = this.internalProgress - 120.0f;
+        float internalLength = this.internalLength + ARC_MIN_LENGTH;
+
+
+        canvas.drawArc(this.externalRectF, externalFrom, externalLength, false, this.externalPaint);
+        canvas.drawArc(this.internalRectF, internalFrom, internalLength, false, this.internalPaint);
+        this.invalidate();
     }
 
 
@@ -137,6 +156,7 @@ public class EasyArcLoading extends View {
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         this.setupRectF();
+        this.invalidate();
     }
 
     private void setupRectF() {
@@ -159,40 +179,6 @@ public class EasyArcLoading extends View {
         this.internalRectF.bottom = this.externalRectF.bottom - arcPadding;
     }
 
-
-    private void play() {
-        ValueAnimator exAnimator = ValueAnimator.ofObject(new ArcEvaluator(), this.decreaseArc);
-        exAnimator.setDuration(800);
-        exAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                ArcProperty current = (ArcProperty) animation.getAnimatedValue();
-                currentExternalStart = current.startAngle;
-                currentExternalSweep = current.sweepAngle;
-                invalidate();
-            }
-        });
-        exAnimator.setRepeatCount(ValueAnimator.INFINITE);
-        exAnimator.setRepeatMode(ValueAnimator.RESTART);
-        exAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
-        exAnimator.start();
-
-        ValueAnimator inAnimator = ValueAnimator.ofObject(new ArcEvaluator(), this.increaseArc);
-        inAnimator.setDuration(800);
-        inAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                ArcProperty current = (ArcProperty) animation.getAnimatedValue();
-                currentInternalStart = current.startAngle;
-                currentInternalSweep = current.sweepAngle;
-                invalidate();
-            }
-        });
-        inAnimator.setRepeatCount(ValueAnimator.INFINITE);
-        inAnimator.setRepeatMode(ValueAnimator.RESTART);
-        inAnimator.setInterpolator(new AccelerateInterpolator());
-        inAnimator.start();
-    }
 
     public float dp2px(float dp) {
         return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, this.getResources().getDisplayMetrics());
@@ -225,10 +211,49 @@ public class EasyArcLoading extends View {
     }
 
 
-    @Override
-    protected void onFinishInflate() {
-        super.onFinishInflate();
-        this.play();
+    private void updateLength(long timeInterval) {
+        if (this.externalIncreaseTime >= EXTERNAL_INCREASE_MAX_TIME) {
+            this.externalPastTime += timeInterval;
+            if (this.externalPastTime > EXTERNAL_ANIMATION_TIME) {
+                this.externalPastTime -= EXTERNAL_ANIMATION_TIME;
+                this.externalIncreaseTime = 0;
+                this.externalIncrease = !this.externalIncrease;
+            }
+            float distance = (float) Math.cos((this.externalPastTime / EXTERNAL_ANIMATION_TIME + 1) * Math.PI) / 2 + 0.5f;
+            float lengthRange = (ARC_MAX_LENGTH - ARC_MIN_LENGTH);
+
+            if (this.externalIncrease) {
+                this.externalLength = distance * lengthRange;
+            } else {
+                float decreaseLength = lengthRange * (1 - distance);
+                this.externalProgress += (this.externalLength - decreaseLength);
+                this.externalLength = decreaseLength;
+            }
+        } else {
+            this.externalIncreaseTime += timeInterval;
+        }
+
+        if (this.internalIncreaseTime >= INTERNAL_INCREASE_MAX_TIME) {
+            this.internalPastTime += timeInterval;
+            if (this.internalPastTime > INTERNAL_ANIMATION_TIME) {
+                this.internalPastTime -= INTERNAL_ANIMATION_TIME;
+                this.internalIncreaseTime = 0;
+                this.internalIncrease = !this.internalIncrease;
+            }
+            float distance = (float) Math.cos((this.internalPastTime / EXTERNAL_ANIMATION_TIME + 1) * Math.PI) / 2 + 0.5f;
+            float lengthRange = (ARC_MAX_LENGTH - ARC_MIN_LENGTH);
+
+            if (this.internalIncrease) {
+                this.internalLength = distance * lengthRange;
+            } else {
+                float decreaseLength = lengthRange * (1 - distance);
+                this.internalProgress += (this.internalLength - decreaseLength);
+                this.internalLength = decreaseLength;
+            }
+        } else {
+            this.internalIncreaseTime += timeInterval;
+        }
     }
+
 
 }
